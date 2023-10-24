0746a3569a0b443c319fb520b4a0345843efa505
Charanel Gile <charanelgile@icloud.com>
2023-10-24 08:43:35 +0800

    Update Get All Products Controller
    - Implemented Searching, Filtering, Sorting,
    Pagination, and Limiting into the Get All Products
    Controller when sending requests with parameters

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,7 +3,114 @@ const { StatusCodes } = require("http-status-codes");
 
 // Get All Products
 const getAllProducts = async (req, res) => {
-  res.status(200).send("Get All Products");
+  // Ensures that only these queries will be processed
+  // Any other queries aside from the ones below will be ignored
+  const {
+    name,
+    description,
+    category,
+    subcategory,
+    filters,
+    sort,
+    fields,
+  } = req.query;
+
+  // Setup an empty queries object
+  let queries = {};
+
+  if (name) {
+    queries.name = { $regex: name, $options: "i" };
+  }
+
+  if (description) {
+    queries.description = { $regex: description, $options: "i" };
+  }
+
+  if (category) {
+    queries.category = category;
+  }
+
+  if (subcategory) {
+    queries.subcategory = subcategory;
+  }
+
+  // Filter the results based on the specified conditions
+  if (filters) {
+    // Map out the basic operators to their corresponding mongoose counterparts
+    const operators = {
+      "=": "$eq",
+      ">": "$gt",
+      "<": "$lt",
+      ">=": "$gte",
+      "<=": "$lte",
+    };
+
+    // RegEx to extract the operator symbols from the request
+    const regex = /\b(>=|>|=|<|<=)\b/g;
+
+    // Replace any matching operator symbol with a pattern that uses its mongoose counterpart: -$<mongoose_operator>-
+    let numericFilters = filters.replace(regex, (match) => {
+      return `-${operators[match]}-`;
+    });
+
+    // Define the filter options where the numeric filters can be used
+    const filterOptions = ["price", "stock", "rating"];
+
+    // Split the numeric filters at every "," then
+    // Iterate over the resulting array of filter options
+    numericFilters = numericFilters.split(",").forEach((option) => {
+      // Split the filter option at every "-" then
+      // Destructure them into field, operator, and value
+      const [field, operator, value] = option.split("-");
+
+      if (filterOptions.includes(field)) {
+        // If the destructured field matches any of the filter options (price, stock, or rating),
+        // add it as a property of the queries object
+        // Example:  queries.price: { $gte: 35 }
+        queries[field] = {
+          [operator]: Number(value),
+        };
+      }
+    });
+  }
+
+  // If nothing was destructured from req.query, then
+  // the find() method will only be receiving an empty object,
+  // therefore returning all products, instead of throwing an error
+  let results = Product.find(queries);
+
+  // Sort the results based on the given options
+  // Otherwise, set "createdAt" as the default sort order
+  if (sort) {
+    const sortOptions = sort.split(",").join(" ");
+    results = results.sort(sortOptions);
+  } else {
+    results = results.sort("createdAt");
+  }
+
+  // Show only the specified / selected fields
+  if (fields) {
+    const selectedFields = fields.split(",").join(" ");
+    results = results.select(selectedFields);
+  }
+
+  // Set Page (defaults to 1, when not specified)
+  const page = Number(req.query.page) || 1;
+  // Set Limit (defaults to 10, when not specified)
+  const limit = Number(req.query.limit) || 10;
+
+  // Calculate how many items will be skipped based on the specified page and limit
+  // This will determine the pagination
+  const skip = (page - 1) * limit;
+
+  results = results.skip(skip).limit(limit);
+
+  const products = await results;
+
+  res.status(StatusCodes.OK).json({
+    count: products.length,
+    products,
+  });
 };
 
 // Get Product
